719a31bc1c1cb8407fd7d0363ec5a25190e6a2fc
Robert Oostenveld <r.oostenveld@donders.ru.nl>
2018-11-20 14:16:42 +0100

    added images to the asset folder, updated publish_website script

--- _scripts/publish_website.sh
+++ _scripts/publish_website.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
 
-if [[ ! -e _config.yml ]] ; then echo ERROR this should be executed in the toplevel directory ;  exit 1 ; fi
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.rvm/rubies/ruby-2.5.3/bin"
+export PATH="$PATH:$HOME/.rvm/gems/ruby-2.5.3/bin"
+export GEM_PATH="/home/mrphys/roboos/.rvm/gems/ruby-2.5.3:/home/mrphys/roboos/.rvm/gems/ruby-2.5.3@global"
+export GEM_HOME="/home/mrphys/roboos/.rvm/gems/ruby-2.5.3"
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-rsync -arpv --delete _site/* fieldtrip:/var/www/fieldtrip.fcdonders.nl_07
+# this runs from a cron-job, so paths are not set as in an interactive terminal
+LOGFILE=$HOME/log/website.log
+GIT=/usr/bin/git
+BUNDLE=$HOME/.rvm/rubies/ruby-2.5.3/bin/bundle
+CP=/usr/bin/cp
+
+[ -e $LOGFILE ] || touch $LOGFILE
+
+cd $HOME/website
+
+# ensure that we have the latest version of the repository
+$GIT pull > /dev/null 2>&1
+
+# compare the latest to the previous version
+PREVIOUS=$(cat $LOGFILE)
+LATEST=$(git log -1 --format=%H)
+if [ "$LATEST" != "$PREVIOUS" ] ; then
+
+echo BUILDING WEBSITE VERSION $LATEST
+echo $LATEST > $LOGFILE
+
+# add a link to the latest commit to the website
+cat << EOF > commit.md
+---
+title: Latest documentation commit
+layout: default
+tags: development
+---
+[$LATEST](https://github.com/fieldtrip/website/commit/$LATEST)
+EOF
+
+$BUNDLE exec jekyll build > /dev/null 2>&1
+
+# copy some static assets over to the root directory
+$CP assets/root/* _site/
+
+else
+# update the timestamp of the logfile, to check when the script was executed 
+touch $LOGFILE
+fi
+
