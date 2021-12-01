2d1a46b26a25f9249ae7a36843a9765790feffdb
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2021-12-01 08:45:57 +0100

    ENH - some updates, still failing

--- example/failed_example_source_statistics.m
+++ example/failed_example_source_statistics.m
@@ -1,4 +1,4 @@
-function functionname
+function test_example_source_statistics
 
 % MEM 4gb
 % WALLTIME 00:10:00
@@ -15,8 +15,13 @@ function functionname
 % To evaluate the reliability of the activation by a statistical measure we will calculate a dependent samples t statistic and plot t maps. Since we are testing for each grid point separately, we have to deal with the multiple comparison problem. One way to control for this is by using a cluster based randomization test. This is further explained in the cluster permutation tutorials for [ERFs](/tutorial/cluster_permutation_timelock) and [time frequency data](/tutorial/cluster_permutation_freq). We will now show how to use a cluster based multiple comparison correction on data for one subjec
 %
 % run sourcestatistics using cluster based correction %
+
+% JM: load a source structure
+load(dccnpath('/home/common/matlab/fieldtrip/data/test/latest/source/meg/source_grid_timelock_trl_LCMV_keepall_rawtrial_ctf151.mat'));
+
+design = [ones(1,floor(numel(source.trial)/2)) 2*ones(1,ceil(numel(source.trial)/2))];
+
 cfg = [];
-cfg.dim         = source.dim;
 cfg.method      = 'montecarlo';
 cfg.statistic   = 'ft_statfun_indepsamplesT';
 cfg.parameter   = 'pow';
@@ -24,10 +29,8 @@ cfg.correctm    = 'cluster';
 cfg.numrandomization = 1000;
 cfg.alpha       = 0.05; % note that this only implies single-sided testing
 cfg.tail        = 0;
-cfg.design(1,:) = [1:length(find(design==1)) 1:length(find(design==2))];
-cfg.design(2,:) = design;
-cfg.uvar        = 1; % row of design matrix that contains unit variable (in this case: trials)
-cfg.ivar        = 2; % row of design matrix that contains independent variable (the conditions)
+cfg.design      = design;
+cfg.ivar        = 1; % row of design matrix that contains independent variable (the conditions)
 
 stat = ft_sourcestatistics(cfg, source);
 
@@ -36,15 +39,18 @@ stat = ft_sourcestatistics(cfg, source);
 % Now the statistical values can be plotted on the subject's MRI, using _stat.mask_ to mask the data, meaning that only significant voxels are plotted.
 %
 % interpolate the t maps to the structural MRI of the subject %
+
+% JM added for the function
+filename_mri = ft_read_mri(dccnpath('/home/common/matlab/fieldtrip/data/Subject01resliced.nii'));
+
 cfg = [];
-cfg.parameter = 'all';
-statplot = ft_sourceinterpolate(cfg, stat, 'subject.mri');
+cfg.parameter = 'stat';
+statplot = ft_sourceinterpolate(cfg, stat, filename_mri);
 
 % plot the t values on the MRI %
 cfg = [];
 cfg.method        = 'slice';
 cfg.funparameter  = 'stat';
-cfg.maskparameter = 'mask';
 figure
 ft_sourceplot(cfg, statplot);
 
