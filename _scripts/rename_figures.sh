da974abcfaa0efe9917d4e6e068a8bca2fc46402
Robert Oostenveld <r.oostenveld@gmail.com>
2022-07-15 09:32:50 +0200

    #576 - created script to make consistent figure names in the assets/img directory and in the markdown pages

---
+++ _scripts/rename_figures.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Use as:
+#   rename_figures.sh <page.md>
+#
+# This script does not do the conversion itself, but prints the commands to screen.
+# The user of the script shoudl check teh commands prior to execution
+
+[ -e "$1" ]  || exit 1
+
+figures=$(grep "include image" $1  | cut -d \" -f 2)
+
+echo $figures
+
+n=1
+for figure in $figures ; do
+  oldname=$(basename $figure)
+  ext=$(echo $oldname | cut -f 2 -d '.')
+  newname=$(dirname $figure)/figure"$n"."$ext"
+  echo git mv ."$figure" ."$newname"
+  echo sed -i .bak s~$figure~$newname~g $1
+  n=$(expr $n + 1)
+done
