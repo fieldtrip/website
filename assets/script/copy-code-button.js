30761f60bc9ba0330dc0febdf6ae6bf54d56dcf0
Robert Oostenveld <r.oostenveld@gmail.com>
2023-08-22 12:24:09 +0200

    fix #700 - added copy button to all code blocks

---
+++ assets/script/copy-code-button.js
@@ -0,0 +1,25 @@
+var codeBlocks = document.querySelectorAll('pre.highlight');
+
+console.log(codeBlocks);
+
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
+
+  codeBlock.prepend(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerText = 'Copied';
+    var fourSeconds = 4000;
+
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+    }, fourSeconds);
+  });
+});
