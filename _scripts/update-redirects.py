af4c2d0ee24667cf00968f5141b838807111f09b
Robert Oostenveld <r.oostenveld@gmail.com>
2025-09-19 07:57:53 +0100

    spelling

--- _scripts/update-redirects.py
+++ _scripts/update-redirects.py
@@ -10,6 +10,7 @@
 #   pip install python-frontmatter pyyaml
 
 import os
+import re
 import frontmatter
 import yaml
 
@@ -27,25 +28,30 @@ for (root,dirs,files) in os.walk(rootdir, topdown=True):
                 for old_link in page['redirect_from']:
                     if old_link.endswith("/"):
                         old_link = old_link[:-1] # remove trailing slash
-                    new_link = os.path.join(root,file[:-3]) # minus the .md
-                    new_link = new_link[len(rootdir):] # minus the rootdir
+                    new_link = os.path.join(root, file[:-3])
+                    new_link = os.path.relpath(new_link, rootdir)
+                    new_link = "/" + new_link.replace(os.sep, "/")
                     print(f"Redirecting {old_link} to {new_link}")
                     redirects[old_link] = new_link
 
 # pass 2: update markdown files
 for (root,dirs,files) in os.walk(rootdir, topdown=True):
     for file in files:
-        if file.endswith(".md"):
+        if file.endswith(".md") and not '.' in root: # skip hidden directories, like .git and .bundle
             filepath = os.path.join(root, file)
-            print(f"Updating links in {filepath}")
             with open(filepath, "r") as f:
                 content = f.read()
+            original_content = content
+
             # Update the content with the new links
             for old_link, new_link in redirects.items():
-                # only change those occurrences that are internal markdown-formatted links
-                old_link = f']({old_link})'
-                new_link = f']({new_link})'
+                old_link = f']({old_link}'
+                new_link = f']({new_link}'
                 content = content.replace(old_link, new_link)
-            with open(filepath, "w") as f:
-                f.write(content)
 
+            if content != original_content:
+                print(f"Updating links in {filepath}")
+                with open(filepath, "w")  as fw:
+                    fw.write(content)
+            
+print("Done.")
